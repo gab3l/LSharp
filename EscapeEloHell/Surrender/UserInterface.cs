2636495d32ac8d443f4e93c39540f137acbc3a4b
gab3l <dfgetet45@web.de>
2015-02-04 17:55:23 +0100

    CC

--- EscapeEloHell/Surrender/UserInterface.cs
+++ EscapeEloHell/Surrender/UserInterface.cs
@@ -13,18 +13,10 @@ namespace Surrender
 
         internal static bool IsEnabled { get { return Menu.Item("enabled").GetValue<bool>(); } }
 
-        //internal static bool IsStartMessageAll { get { return Menu.Item("startmessageall").GetValue<bool>(); } }
-
         internal static void CreateMenu()
         {
             Menu = new Menu("Surrender", "surrender", true);
-
             Menu.AddItem(new MenuItem("enabled", "Enable").SetValue(true));
-
-            //// SubMenu Configuration
-            //var submenu = Menu.AddSubMenu(new Menu("Configuration", "configuration"));
-            //submenu.AddItem(new MenuItem("startmessageall", "StartMessage All").SetValue(true));
-         
             Menu.AddToMainMenu();
         }
 
