0afdb8df451d2c24529541745c7c86c7c7ce29fe
gab3l <dfgetet45@web.de>
2015-04-03 18:40:22 +0200

    gg

--- EscapeEloHell/UseTeleportWithFullHealth/Game.cs
+++ EscapeEloHell/UseTeleportWithFullHealth/Game.cs
@@ -24,15 +24,15 @@ namespace UseTeleportWithFullHealth
             teleportingSpell = ObjectManager.Player.GetSpellSlot("SummonerTeleport");
             //LeagueSharp.Game.PrintChat("hero.HealthPercent, hero.ManaPercent" + hero.MaxHealth / hero.Health + "  " +  hero.MaxMana / hero.Mana);
             //LeagueSharp.Game.PrintChat("Teleport=" + Teleport.IsStart(hero.HealthPercent, hero.ManaPercent).ToString());
-            if (Teleport.IsStart(hero.Health / hero.MaxHealth, hero.Mana / hero.MaxMana) && teleportingSpell.IsReady())
+            if (Teleport.IsStart((hero.Health / hero.MaxHealth) * 100, (hero.Mana / hero.MaxMana)*100) && teleportingSpell.IsReady())
             {
                 var results =
                               ObjectManager.Get<Obj_AI_Base>()
                                   .Where(
                                       x =>
-                                          Math.Abs(x.Position.X - LeagueSharp.Game.CursorPos.X) < 20 &&
-                                          Math.Abs(x.Position.Y - LeagueSharp.Game.CursorPos.Y) < 20 &&
-                                          Math.Abs(x.Position.Z - LeagueSharp.Game.CursorPos.Z) < 20);
+                                          Math.Abs(x.Position.X - LeagueSharp.Game.CursorPos.X) < 2000 &&
+                                          Math.Abs(x.Position.Y - LeagueSharp.Game.CursorPos.Y) < 2000 &&
+                                          Math.Abs(x.Position.Z - LeagueSharp.Game.CursorPos.Z) < 2000);
                 foreach (var item in results)
                 {
                     if (!ObjectManager.Player.Spellbook.IsCastingSpell)
