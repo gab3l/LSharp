7d6c60b28ad96d4da74acdfc89c79b1b79e6de56
gab3l <dfgetet45@web.de>
2015-04-03 18:53:18 +0200

    added minions

--- EscapeEloHell/UseTeleportWithFullHealth/Game.cs
+++ EscapeEloHell/UseTeleportWithFullHealth/Game.cs
@@ -22,8 +22,8 @@ namespace UseTeleportWithFullHealth
 
             var hero = ObjectManager.Player;
             teleportingSpell = ObjectManager.Player.GetSpellSlot("SummonerTeleport");
-            //LeagueSharp.Game.PrintChat("hero.HealthPercent, hero.ManaPercent" + hero.MaxHealth / hero.Health + "  " +  hero.MaxMana / hero.Mana);
-            //LeagueSharp.Game.PrintChat("Teleport=" + Teleport.IsStart(hero.HealthPercent, hero.ManaPercent).ToString());
+          
+
             if (Teleport.IsStart((hero.Health / hero.MaxHealth) * 100, (hero.Mana / hero.MaxMana)*100) && teleportingSpell.IsReady())
             {
                 var results =
@@ -40,6 +40,22 @@ namespace UseTeleportWithFullHealth
                         ObjectManager.Player.Spellbook.CastSpell(teleportingSpell, item);
                     } 
                 }
+
+                var minions =
+                           ObjectManager.Get<Obj_AI_Minion>()
+                               .Where(
+                                   x =>
+                                       Math.Abs(x.Position.X - LeagueSharp.Game.CursorPos.X) < 20 &&
+                                       Math.Abs(x.Position.Y - LeagueSharp.Game.CursorPos.Y) < 20 &&
+                                       Math.Abs(x.Position.Z - LeagueSharp.Game.CursorPos.Z) < 20);
+                foreach (var item in minions)
+                {
+                    if (!ObjectManager.Player.Spellbook.IsCastingSpell)
+                    {
+                        ObjectManager.Player.Spellbook.CastSpell(teleportingSpell, item);
+                    }
+                }
+
             }
         }
     }
