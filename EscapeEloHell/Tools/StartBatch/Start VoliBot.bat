b6ed1e9309e2dee8c8d736d4964a8632d46aef1d
gab3l <dfgetet45@web.de>
2015-04-08 15:05:31 +0200

    bugfix

--- EscapeEloHell/Tools/StartBatch/Start VoliBot.bat
+++ EscapeEloHell/Tools/StartBatch/Start VoliBot.bat
@@ -1,5 +1,5 @@
 SET path=C:\Riot Games\League of Legends
 SET Volipath=C:\Users\NEWNICK\Desktop\MyVoliBots\Bot100
-echo start "" "%Volipat%\VoliBot.exe"
+start "" "%Volipat%\VoliBot.exe"
 xcopy  /R /Y "%path%\Config\game.cfg"  "%path%\Config\gameOrginal.cfg*" 
 xcopy  /R /Y "%path%\Config\gameBot.cfg"  "%path%\Config\game.cfg*" 
